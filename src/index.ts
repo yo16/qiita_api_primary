4b9189f6ea270b605613a15b985eee10fe6c7eda
yo16 <xxyo1xx@gmail.com>
2025-10-17 15:16:52 +0900

    全件取得してCSVに保存する機能を追加

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,104 @@
 import axios from "axios";
 import dotenv from "dotenv";
+import fs from "fs";
 
 dotenv.config();
 
 const token = process.env.QIITA_TOKEN;
+const PER_PAGE = 100;
 
-async function getQiitaItems() {
+
+// ほしい情報の型定義
+interface QiitaItem {
+  title: string;
+  url: string;
+  page_views_count: number;
+  likes_count: number;
+  stocks_count: number;
+  comments_count: number;
+  tags: string[];
+  created_at: string;
+  updated_at: string;
+}
+
+
+// 記事を取得する関数
+async function fetchQiitaItems(): Promise<QiitaItem[]> {
+  const allItems: QiitaItem[] = [];
+  let page = 1;
+
   try {
-    const response = await axios.get("https://qiita.com/api/v2/items", {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-      params: {
-        per_page: 5, // 取得件数
-      },
-    });
+    // 無限ループで記事を取得
+    while (true) {
+      // APIを呼び出して、Qiitaの最新記事を取得
+      const response = await axios.get("https://qiita.com/api/v2/authenticated_user/items", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        params: {
+          per_page: PER_PAGE,
+          page: page,
+        },
+      });
 
-    console.log("Qiita最新記事:");
-    response.data.forEach((item: any) => {
-      console.log(`- ${item.title} (${item.url})`);
-    });
+      const currentItems = response.data.map((item:any) => ({
+        title: item.title,
+        url: item.url,
+        page_views_count: item.page_views_count,
+        likes_count: item.likes_count,
+        comments_count: item.comments_count,
+        stocks_count: item.stocks_count,
+        tags: item.tags.map((tag: any) => tag.name),
+        created_at: item.created_at,
+        updated_at: item.updated_at,
+      }));
+
+      allItems.push(...currentItems);
+
+      if (response.data.length < PER_PAGE) {
+        break;
+      }
+      page++;
+    }
   } catch (error) {
     console.error("API呼び出しエラー:", error);
   }
+
+  console.log(`✅ 合計 ${allItems.length} 件取得完了`);
+  return allItems;
 }
 
-getQiitaItems();
+
+// CSVファイルに保存する関数
+function saveToCSV(items: QiitaItem[]): void {
+  // ,でjoinして、それを\nでjoin
+  const csv = items.map((item) => (
+    [
+      item.title,
+      item.url,
+      item.page_views_count,
+      item.likes_count,
+      item.comments_count,
+      item.stocks_count,
+      item.tags.join(","),
+      item.created_at,
+      item.updated_at,
+    ].join(",")
+  )).join("\n");
+
+  // ファイルに保存
+  fs.writeFileSync("qiita_items.csv", csv);
+}
+
+
+async function main() {
+  const items = await fetchQiitaItems();
+  if (items) {
+    saveToCSV(items);
+    console.log("✅ CSVファイルに保存完了");
+  } else {
+    console.error("❌ 記事の取得に失敗しました");
+  }
+}
+
+main();
